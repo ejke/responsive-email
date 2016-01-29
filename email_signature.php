5b7a8e00642a7136a45e00cb20157365be02f137
ejke <eikejraielo@gmail.com>
2016-01-29 16:26:05 +0200

    Create email_signature.php

---
+++ email_signature.php
@@ -0,0 +1,20 @@
+<?php
+
+<table cellpadding=0 cellspacing=0 style="margin: 0 auto; padding: 12px 0;"><tr><td>
+	<tr>
+		<td style="width:155px;">
+			<img src=""; style=" display:block; border:0;">
+		</td> 
+		<td style="font-family:Verdana;">
+			<span style="font-size:10pt"><span style="font-weight:bold; color:#FF5800;">First name Last name</span> | occupation</span><br><br>
+            <span style="font-size:8pt">Company name<br>                  
+			Address street 14, City Postcode<br>
+			Office <a style="text-decoration: none; color:#000;" href="tel:+00 0000000" value="+00 000 0000" target="_blank">+00 000 0000</a> | 
+				<a style="text-decoration: none; color:#000;" href="mailto:firstname.lastname@email.mail" target="_blank">firstname.lastname@email.mail</a><br>
+			Mobile <a style="text-decoration: none; color:#000;" href="tel:+00 0000000" value="+00 000 0000" target="_blank">+00 000 0000</a><br>
+			<a href="www.lennujaam24.ee">www.yourdomain.com</a>
+         </td>
+	</tr>    
+</td></tr></table>
+
+?>
